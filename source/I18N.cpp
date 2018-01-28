dc6b7e390f45f13d7cec7c1f5ccdff3706d5c1b5
予顺 刘 <yshliu0321@icloud.com>
2018-01-28 20:24:49 +0800

    ReImpl

--- source/I18N.cpp
+++ source/I18N.cpp
@@ -23,52 +23,66 @@
 #include <ini.hpp>
 
 namespace {
-    std::map<std::string, std::string> kvs;
-    std::string curLang = "zh_CN";
+    class I18NImpl {
+    public:
+        std::string getCur() const noexcept { return curLang; }
 
-    I18N::LangInfo loadInfo(const std::string& kv) {
-        I18N::LangInfo ret;
-        std::ifstream file("Lang/" + kv + ".ini");
-        if (file.good()) {
-            INI::Parser x(file);
-            ret.id = kv;
-            ret.engId = x.top()("Info")["EngID"];
-            ret.prettyName = x.top()("Info")["Pretty Name"];
+        void setCur(const std::string& nv) {
+            curLang = nv;
+            loadLang(curLang);
         }
-        return ret;
-    }
 
-    bool loadLang(const std::string& lang) {
-        std::ifstream file("Lang/" + lang + ".ini");
-        if (file.good()) {
-            INI::Parser map(file);
-            kvs = std::move(map.top()("Translations").values);
-            curLang = lang;
-            return true;
+        const std::string& get(const std::string& key) {
+            auto iter = kvs.find(key);
+            if (iter == kvs.end())
+                iter = kvs.insert({ key, key }).first;
+            return iter->second;
         }
-        return false;
-    }
-}
 
-std::string I18N::getCurLang() noexcept { return curLang; }
-
-void I18N::setCurLang(const std::string& nv) {
-    curLang = nv;
-    loadLang(curLang);
-}
-
-const std::string& I18N::get(const std::string& key) {
-    auto iter = kvs.find(key);
-    if (iter == kvs.end())
-        iter = kvs.insert({ key, "" }).first;
-    return iter->second;
-}
-
-void I18N::iterate(std::function<void(const LangInfo&)> fcn) {
-    for (auto& x : filesystem::directory_iterator("Lang"))
-        if (x.path().extension().compare("ini")) {
-            std::string name = x.path().filename().string();
-            const auto info = loadInfo(name.substr(0, name.length() - 4));
-            fcn(info);
+        static void iterate(std::function<void(const I18N::LangInfo&)> fcn) {
+            for (auto& x : filesystem::directory_iterator("Lang"))
+                if (x.path().extension().compare("ini")) {
+                    std::string name = x.path().filename().string();
+                    const auto info = loadInfo(name.substr(0, name.length() - 4));
+                    fcn(info);
+                }
         }
+        static auto& getInstance() noexcept {
+            static I18NImpl impl{};
+            return impl;
+        }
+    private:
+        static I18N::LangInfo loadInfo(const std::string& kv) {
+            I18N::LangInfo ret;
+            std::ifstream file("Lang/" + kv + ".ini");
+            if (file.good()) {
+                INI::Parser x(file);
+                ret.id = kv;
+                ret.engId = x.top()("Info")["EngID"];
+                ret.prettyName = x.top()("Info")["Pretty Name"];
+            }
+            return ret;
+        }
+
+        bool loadLang(const std::string& lang) {
+            std::ifstream file("Lang/" + lang + ".ini");
+            if (file.good()) {
+                INI::Parser map(file);
+                kvs = std::move(map.top()("Translations").values);
+                curLang = lang;
+                return true;
+            }
+            return false;
+        }
+        std::map<std::string, std::string> kvs;
+        std::string curLang = "zh_CN";
+    };
 }
+
+std::string I18N::getCurLang() noexcept { return I18NImpl::getInstance().getCur(); }
+
+void I18N::setCurLang(const std::string& nv) { I18NImpl::getInstance().setCur(nv); }
+
+const std::string& I18N::get(const std::string& key) { return I18NImpl::getInstance().get(key); }
+
+void I18N::iterate(std::function<void(const LangInfo&)> fcn) { return I18NImpl::iterate(fcn); }
